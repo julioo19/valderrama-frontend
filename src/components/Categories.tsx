045687b843858100824ba00ff6a33e80dbeaa110
julioo19 <julio030902@gmail.com>
2025-11-23 20:40:21 -0500

    Cambios de color menores

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -75,8 +75,8 @@ const Categories = () => {
     <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-2 
     bg-gray-100 p-2 rounded-lg mb-4 text-sm'>
       {categories.map(category => (
-        <div className={`flex items-center justify-center gap-2 cursor-pointer px-2 py-1 rounded-md 
-          ${category.slug === selectedCategory ? "bg-white font-bold" : "text-primary-light"}
+        <div className={`flex items-center justify-center gap-2 cursor-pointer px-2 py-1 rounded-md hover:text-white hover:bg-green-600 transition-all duration-300
+          ${category.slug === selectedCategory ? "bg-green-600 font-bold text-white" : "text-primary-light"}
         `} key={category.name}
         onClick={() => handleChange(category.slug)}
         >
